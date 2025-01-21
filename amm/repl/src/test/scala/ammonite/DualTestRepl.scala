10be97a836ecb75288d907bdc1655611b9f7b424
aldum <aldum@artixlinux.org>
2025-01-21 16:58:05 +0100

    refactor: typo

--- amm/repl/src/test/scala/ammonite/DualTestRepl.scala
+++ amm/repl/src/test/scala/ammonite/DualTestRepl.scala
@@ -34,8 +34,8 @@ class DualTestRepl { dual =>
     else scalaVersion.takeWhile(_ != '.')
   def scala2 = scalaVersion.startsWith("2.")
   def scala2_12 = scalaVersion.startsWith("2.12.")
-  def scala3_5_1OrHigher =
   def scala2_13_16 = scalaVersion.startsWith("2.13.16")
+  def scala3_5_1OorHigher =
     (scalaVersion.startsWith("3.5.") && scalaVersion.split("\\.")(2).toInt >= 1) ||
       scalaVersion.split("\\.")(1).toInt >= 6
 
